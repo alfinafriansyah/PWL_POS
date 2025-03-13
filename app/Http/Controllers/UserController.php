50ce4063d5d706055b68e4c2333cc9cd9ba857f7
afriansyah <alfinafriansyah68@gmail.com>
2025-03-13 13:49:04 +0700

    Jobsheet 4 - Praktikum 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,9 +46,13 @@ class UserController extends Controller
         //     abort(404);
         // });
 
-        $user = UserModel::findOr(20, ['username', 'nama'], function() {
-            abort(404);
-        });
+        // $user = UserModel::findOr(20, ['username', 'nama'], function() {
+        //     abort(404);
+        // });
+
+        // $user = UserModel::findOrFail(1);
+
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
 
         return view('user', ['data' => $user]); 
     }
