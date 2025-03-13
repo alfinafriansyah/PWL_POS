1e33ee625bbe724ec3f5e40c8a38ea42def83487
afriansyah <alfinafriansyah68@gmail.com>
2025-03-13 13:34:57 +0700

    Jobsheet 4 - Praktikum 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,15 +25,31 @@ class UserController extends Controller
         // UserModel::where('username', 'customer-1')->update($data);
 
         // Jobsheet 4
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
 
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::find(1);
+
+        // $user = UserModel::where('level_id', 1)->first();
+
+        // $user = UserModel::FirstWhere('level_id', 1);
+
+        // $user = UserModel::findOr(1, ['username', 'nama'], function() {
+        //     abort(404);
+        // });
+
+        $user = UserModel::findOr(20, ['username', 'nama'], function() {
+            abort(404);
+        });
+
+        return view('user', ['data' => $user]); 
     }
 }
